7b744ae009ec13116116c0e13d4e1764db2440b0
Arman-Aslanyan <67847062+Arman-Aslanyan@users.noreply.github.com>
2021-11-19 15:00:40 -0800

    update Retrying-ish everyting

---
+++ Assets/Scripts/ElemRetry.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElemRetry : MonoBehaviour
+{
+    public bool mine = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    /*[System.Serializable]
+    public class DeclareMultiDimensionalArray
+    {
+        public Element[] elem;
+    }*/
+
+    public ElemRetry[] elems;
+
+    private void Start()
+    {
+        elems = FindObjectsOfType<ElemRetry>();
+    }
+
+    public int AdjacentCount(Vector3 pos)
+    {
+        int mines = 0;
+        if (FindNearbyMines(new Vector3(pos.x - 1, pos.y + 1, pos.z))) //Top-Left
+            mines++;
+        if (FindNearbyMines(new Vector3(pos.x, pos.y + 1, pos.z))) //Top
+            mines++;
+        if (FindNearbyMines(new Vector3(pos.x + 1, pos.y + 1, pos.z))) //Top-right
+            mines++;
+        if (FindNearbyMines(new Vector3(pos.x + 1, pos.y, pos.z))) //Right
+            mines++;
+        if (FindNearbyMines(new Vector3(pos.x + 1, pos.y - 1, pos.z))) //Bottom-Right
+            mines++;
+        if (FindNearbyMines(new Vector3(pos.x, pos.y - 1, pos.z))) //Bottom
+            mines++;
+        if (FindNearbyMines(new Vector3(pos.x - 1, pos.y - 1, pos.z))) //Bottom-Left
+            mines++;
+        if (FindNearbyMines(new Vector3(pos.x - 1, pos.y, pos.z))) //Left
+            mines++;
+
+        return mines;
+    }
+
+    public bool FindNearbyMines(Vector3 pos)
+    {
+        bool isMine = false;
+        foreach(ElemRetry elem in elems)
+        {
+            if (elem.transform.position == pos && elem.mine)
+            {
+                isMine = true;
+            }
+        }
+
+        return isMine;
+    }
+}
